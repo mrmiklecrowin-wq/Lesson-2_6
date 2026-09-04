27ed4da137a9f92cb7f5683d159c77b0d80d1529
Venceey <mr.miklecrowin@gmail.com>
2026-09-04 23:52:07 +0300

    Update Lesson 6_1.cpp

--- Lesson 6_1.cpp
+++ Lesson 6_1.cpp
@@ -1,42 +1,37 @@
 #include <iostream>
+#include <string>
 #include <windows.h>
-#include <string.h>
 
-
-int function(std::string str, int forbidden_length){
-	
-	int str_length = str.size();
-	int result = 0;
-	std::string bad_length = "Вы ввели слово запретной длины! До свидания";
-	
-	if(str_length == forbidden_length){;
-		std::cout << "Длина слова " << str << " равна " << forbidden_length <<std::endl;
-		return result = 0;
+int function(const std::string& str, int forbidden_length) {
+	if ( forbidden_length == static_cast<int>(str.size())){
+		throw std::string ("bad_length");
 	}
-	else {
-		std::cout << bad_length;
-		return result = -1;
-	}
-};
+	return static_cast<int>(str.size());
+}
 
-
-int main(){
-	
+int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	
 	int forbidden_length = 0;
-	std::string str = " ";
-	int result = 0;
+	std::cout << "Введите запретную длину: ";
+	std::cin >> forbidden_length;
 	
-	while(result == 0){
-		
-		std::cout <<"Введите запретную длину: ";
-		std::cin >> forbidden_length;
-		std::cout << "Введите слово: " ;
+	while (true) {
+		std::string str;
+		std::cout << "Введите слово: ";
 		std::cin >> str;
-		result = function(str,forbidden_length);
 		
+		try {
+			int str_length = function(str, forbidden_length);
+			std::cout << "Длина слова " << str << " равна " << str_length << std::endl;
+		} catch (std::string bad_length) {
+			if (bad_length== "bad_length") {
+				std::cout << "Вы ввели слово запретной длины! До свидания" << std::endl;
+				break;
+			}
+		}
 	}
-	return result;
+	
+	return 0;
 }
